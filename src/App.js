1f2b1dafa562a6ab12bf017c775c737be552e06b
Emon Ahmed <technicalboyemon@gmail.com>
2021-09-26 00:07:48 +0600

    Check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Emon Ahmed
+          Emon Ahmed || JS Military
         </p>
       </header>
     </div>
